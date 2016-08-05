192d7fd9ec090722161b2708880c3c58423f3367
regstrtn <regstrtn@gmail.com>
2016-08-05 23:06:50 +0530

    Add a basic vimrc for c header files and a file for initialising c headers

---
+++ .vimrc
@@ -0,0 +1,2 @@
+set number
+autocmd BufNewFile *.c r /users/user/hdrs.c
---
+++ hdrs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+
+/******************************************************************
+ * Filename: 
+ * Created by: Mohammad Luqman
+ *
+ *
+ * ****************************************************************/
+
+int main(int argc, char **argv{
+
+	return 0;
+}
